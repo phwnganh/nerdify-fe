01d95fb1e82d779a62e210f95a5c36c1f16582fb
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-01 10:48:12 +0700

    Update LearnerLayout.js

--- frontend/src/layouts/LearnerLayout.js
+++ frontend/src/layouts/LearnerLayout.js
@@ -1,52 +1,3 @@
-// import React from "react";
-// import { Divider } from "antd";
-// import LearnerHeader from "../components/Header/LearnerHeader";
-// import Footer from "../components/Footer";
-
-// // LearnerLayout Component
-// export const LearnerLayout = ({ children }) => {
-//   return (
-//     <>
-//       {/* Main Container */}
-//       <div style={{ minHeight: "100vh", width: "100%" }}>
-//         {/* Fixed LearnerHeader */}
-//         <LearnerHeader />
-
-//         {/* Content Area */}
-//         <div style={styles.container}>
-//           <div style={styles.content}>{children}</div>
-
-//           {/* Divider and Footer */}
-//           <Divider style={{ marginBottom: 5 }} />
-//           <Footer />
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// // Styles
-// const styles = {
-//   container: {
-//     display: "flex",
-//     flexDirection: "column",
-//     minHeight: "100vh",
-//     width: "100%",
-//     paddingTop: "80px",
-//     boxSizing: "border-box",
-//     overflowX: "hidden",
-//   },
-//   content: {
-//     flex: 1,
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     width: "100%",
-//   },
-// };
-
-// export default LearnerLayout;
-
 import React from "react";
 import { Divider } from "antd";
 import LearnerHeader from "../components/Header/LearnerHeader";
@@ -80,19 +31,19 @@ const styles = {
     display: "flex",
     flexDirection: "column",
     minHeight: "100vh",
-    width: "100%", // Chiều rộng container luôn là 100% màn hình
-    maxWidth: "1200px", // Đặt maxWidth cố định cho container (ví dụ: 1200px)
-    margin: "0 auto", // Căn giữa container nếu có không gian trống
-    paddingTop: "80px", // Đảm bảo không gian cho header cố định
-    boxSizing: "border-box", // Đảm bảo padding không ảnh hưởng đến kích thước tổng
-    overflowX: "hidden", // Ngăn chặn cuộn ngang
+    width: "100%",
+    maxWidth: "1200px",
+    margin: "0 auto",
+    paddingTop: "80px",
+    boxSizing: "border-box",
+    overflowX: "hidden",
   },
   content: {
     flex: 1,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    width: "100%", // Nội dung sẽ chiếm toàn bộ chiều rộng của container
+    width: "100%",
   },
 };
 
