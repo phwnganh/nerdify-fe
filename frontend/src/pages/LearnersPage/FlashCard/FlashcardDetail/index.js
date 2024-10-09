8fef4454837f57ff777decc77a1424312ad0c016
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-09 09:15:28 +0700

    add card flip react

--- frontend/src/pages/LearnersPage/FlashCard/FlashcardDetail/index.js
+++ frontend/src/pages/LearnersPage/FlashCard/FlashcardDetail/index.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Button, Col, Dropdown, List, Row } from "antd";
+import {
+  Button,
+  Col,
+  Dropdown,
+  List,
+  Row,
+  Modal,
+} from "antd";
 import {
   EditOutlined,
   FileOutlined,
@@ -17,20 +24,21 @@ import { CLIENT_URI } from "../../../../constants/uri.constants";
 import { useNavigate, useParams } from "react-router-dom";
 import ButtonCustom from "../../../../components/Button";
 import { TextCustom, TitleCustom } from "../../../../components/Typography";
-
 import { Container } from "./styled";
 import BreadCrumbHome from "../../../../components/BreadCrumb/BreadCrumbHome";
 import ModalCustom from "../../../../components/Modal";
 import InputCustom from "../../../../components/Input";
+import ReactCardFlip from 'react-card-flip';
+
 export default function FlashCardDetail() {
   const navigate = useNavigate();
   const { flashcardId } = useParams();
+
   const [flashcard, setFlashcard] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlippedNormal, setIsFlippedNormal] = useState(false);
+  const [isFlippedModal, setIsFlippedModal] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
-
-  // flashcard test modal
   const [open, setOpen] = useState(false);
   const [numberOfCard, setNumberOfCard] = useState("");
 
@@ -57,12 +65,20 @@ export default function FlashCardDetail() {
   const items = [
     {
       key: "1",
-      label: <div onClick={() => navigate(CLIENT_URI.CREATE_FLASH_CARD)}>Tạo folder mới</div>,
+      label: (
+        <div onClick={() => navigate(CLIENT_URI.CREATE_FLASH_CARD)}>
+          Tạo folder mới
+        </div>
+      ),
       icon: <FolderOutlined />,
     },
     {
       key: "2",
-      label: <div onClick={() => navigate(CLIENT_URI.CREATE_FLASH_CARD)}>Tạo học phần mới</div>,
+      label: (
+        <div onClick={() => navigate(CLIENT_URI.CREATE_FLASH_CARD)}>
+          Tạo học phần mới
+        </div>
+      ),
       icon: <FileOutlined />,
     },
   ];
@@ -81,23 +97,24 @@ export default function FlashCardDetail() {
   ];
 
   const handleNext = () => {
-    setIsFlipped(false);
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % flashcard?.cards.length);
-    console.log(isFlipped);
+    setIsFlippedNormal(false);
+    setIsFlippedModal(false);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex + 1) % flashcard?.cards.length
+    );
   };
 
   const handlePrevious = () => {
-    setIsFlipped(false);
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + flashcard?.cards.length) % flashcard?.cards.length);
-    console.log(isFlipped);
-  };
-
-  const toggleFlashcard = () => {
-    setIsFlipped(!isFlipped);
+    setIsFlippedNormal(false);
+    setIsFlippedModal(false);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + flashcard?.cards.length) % flashcard?.cards.length
+    );
   };
 
   const toggleFullScreen = () => {
     setIsFullScreen(!isFullScreen);
+    setIsFlippedModal(false);
   };
 
   const handleSpeak = (text) => {
@@ -105,9 +122,7 @@ export default function FlashCardDetail() {
     speechSynthesis.speak(utterance);
   };
 
-  //just number no text
   const handleOnChange = (e) => {
-    //validate quantity
     const { value: inputValue } = e.target;
     const reg = /^-?\d*(\.\d*)?$/;
     if (reg.test(inputValue) || inputValue === "" || inputValue === "-") {
@@ -115,32 +130,222 @@ export default function FlashCardDetail() {
     }
   };
 
+  const renderFlashcardContent = (isFlipped, setIsFlipped) => {
+    const toggleFlashcard = () => {
+      setIsFlipped(!isFlipped);
+    };
+
+    return (
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="orientation">
+        {/* Front of the card */}
+        <div
+          key="front"
+          style={{
+            backgroundColor: "#e0e0e0",
+            padding: "20px",
+            borderRadius: "8px",
+            textAlign: "center",
+            cursor: "pointer",
+            transition: "transform 0.6s",
+          }}
+          onClick={toggleFlashcard}
+        >
+          <Row justify={"end"}>
+            <Col>
+              <Button
+                icon={<SoundOutlined />}
+                shape="circle"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleSpeak(flashcard?.cards[currentIndex].terms);
+                }}
+              />
+            </Col>
+          </Row>
+          <div style={{ margin: "40px 0" }}>
+            <TextCustom>{flashcard?.cards[currentIndex].terms}</TextCustom>
+          </div>
+          <Row justify={"space-around"} align={"middle"}>
+            <Col>
+              <Button
+                icon={<LeftOutlined />}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handlePrevious();
+                  setIsFlipped(false);
+                }}
+                shape="circle"
+              />
+            </Col>
+            <Col>{`${currentIndex + 1}/${flashcard?.cards.length}`}</Col>
+            <Col>
+              <Button
+                icon={<RightOutlined />}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleNext();
+                  setIsFlipped(false);
+                }}
+                shape="circle"
+              />
+            </Col>
+          </Row>
+          <Row justify={"end"} align={"end"} style={{ marginTop: "20px" }}>
+            <Col style={{ marginRight: "20px" }}>
+              <Button icon={<ShareAltOutlined />} shape="circle" />
+            </Col>
+            <Col>
+              <Button
+                icon={
+                  isFullScreen ? (
+                    <FullscreenExitOutlined />
+                  ) : (
+                    <FullscreenOutlined />
+                  )
+                }
+                shape="circle"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleFullScreen();
+                }}
+              />
+            </Col>
+          </Row>
+        </div>
+        {/* Back of the card */}
+        <div
+          key="back"
+          style={{
+            backgroundColor: "#e0e0e0",
+            padding: "20px",
+            borderRadius: "8px",
+            textAlign: "center",
+            cursor: "pointer",
+            transition: "transform 0.6s",
+          }}
+          onClick={toggleFlashcard}
+        >
+          <Row justify={"end"}>
+            <Col>
+              <Button
+                icon={<SoundOutlined />}
+                shape="circle"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleSpeak(flashcard?.cards[currentIndex].definitions);
+                }}
+              />
+            </Col>
+          </Row>
+          <div style={{ margin: "40px 0" }}>
+            <TextCustom>{flashcard?.cards[currentIndex].definitions}</TextCustom>
+          </div>
+          <Row justify={"space-around"} align={"middle"}>
+            <Col>
+              <Button
+                icon={<LeftOutlined />}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handlePrevious();
+                  setIsFlipped(false);
+                }}
+                shape="circle"
+              />
+            </Col>
+            <Col>{`${currentIndex + 1}/${flashcard?.cards.length}`}</Col>
+            <Col>
+              <Button
+                icon={<RightOutlined />}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleNext();
+                  setIsFlipped(false);
+                }}
+                shape="circle"
+              />
+            </Col>
+          </Row>
+          <Row justify={"end"} align={"end"} style={{ marginTop: "20px" }}>
+            <Col style={{ marginRight: "20px" }}>
+              <Button icon={<ShareAltOutlined />} shape="circle" />
+            </Col>
+            <Col>
+              <Button
+                icon={
+                  isFullScreen ? (
+                    <FullscreenExitOutlined />
+                  ) : (
+                    <FullscreenOutlined />
+                  )
+                }
+                shape="circle"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  toggleFullScreen();
+                }}
+              />
+            </Col>
+          </Row>
+        </div>
+      </ReactCardFlip>
+    );
+  };
+
   return (
     <div style={{ padding: "24px" }}>
       <div style={{ marginLeft: 235 }}>
         <BreadCrumbHome />
       </div>
-
+      {/* Test Modal */}
       <ModalCustom
         title="Nhập số lượng câu"
-        // visible={isModalVisible}
         open={open}
         onOk={handleOk}
         onCancel={handleCancel}
         width={400}
         footer={[
-          <ButtonCustom key="submit" buttonType="primary" onClick={handleOk} disabled={!(numberOfCard > 0 && numberOfCard <= flashcard?.cards.length)}>
+          <ButtonCustom
+            key="submit"
+            buttonType="primary"
+            onClick={handleOk}
+            disabled={
+              !(
+                numberOfCard > 0 && numberOfCard <= flashcard?.cards.length
+              )
+            }
+          >
             Bắt đầu làm bài
           </ButtonCustom>,
         ]}
         centered
       >
-        <TextCustom style={{ display: "flex", alignItems: "center", type: "number" }}>
+        <TextCustom style={{ display: "flex", alignItems: "center" }}>
           Số lượng câu hỏi : &nbsp;
-          <InputCustom onChange={handleOnChange} style={{ width: "90px", textAlign: "center", padding: "0" }} addonAfter={`/ ${flashcard?.cards.length}`} />
+          <InputCustom
+            onChange={handleOnChange}
+            style={{
+              width: "90px",
+              textAlign: "center",
+              padding: "0",
+            }}
+            addonAfter={`/ ${flashcard?.cards.length}`}
+          />
         </TextCustom>
       </ModalCustom>
-
+      {/* Full Screen Modal */}
+      <Modal
+        visible={isFullScreen}
+        onCancel={() => {
+          toggleFullScreen();
+          setIsFlippedModal(false);
+        }}
+        footer={null}
+        width={"80%"}
+        bodyStyle={{ padding: 0 }}
+        centered
+      >
+        {renderFlashcardContent(isFlippedModal, setIsFlippedModal)}
+      </Modal>
       <div style={{ textAlign: "center", marginBottom: "20px" }}>
         <TitleCustom level={2}>{flashcard?.title}</TitleCustom>
         <div style={{ textAlign: "center", marginBottom: "10px" }}>
@@ -158,7 +363,6 @@ export default function FlashCardDetail() {
             Tạo bộ flashcard mới
           </ButtonCustom>
         </Dropdown>
-
         <ButtonCustom
           style={{
             background: "#088d2b",
@@ -172,83 +376,27 @@ export default function FlashCardDetail() {
         </ButtonCustom>
       </div>
       <Container>
-        <div
-          style={{
-            backgroundColor: "#e0e0e0",
-            padding: "20px",
-            borderRadius: "8px",
-            textAlign: "center",
-            cursor: "pointer",
-            transition: "transform 0.6s",
-            width: isFullScreen ? "70vw" : "auto",
-            height: isFullScreen ? "30vh" : "auto",
-            top: isFullScreen ? "0" : "auto",
-            left: isFullScreen ? "0" : "auto",
-            overflow: "hidden",
-          }}
-          onClick={toggleFlashcard}
-        >
-          <Row justify={"end"}>
-            <Col>
-              <Button
-                icon={<SoundOutlined />}
-                shape="circle"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleSpeak(isFlipped ? flashcard?.cards[currentIndex].definitions : flashcard?.cards[currentIndex].terms);
-                }}
-              ></Button>
-            </Col>
-          </Row>
-          <div style={{ margin: "40px 0" }}>
-            <TextCustom>{isFlipped ? flashcard?.cards[currentIndex].definitions : flashcard?.cards[currentIndex].terms}</TextCustom>
-          </div>
-          <Row justify={"space-around"} align={"middle"}>
-            <Col>
-              <Button
-                icon={<LeftOutlined />}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handlePrevious();
-                }}
-                shape="circle"
-              ></Button>
-            </Col>
-            <Col>{`${currentIndex + 1}/${flashcard?.cards.length}`}</Col>
-            <Col>
-              <Button
-                icon={<RightOutlined />}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleNext();
-                }}
-                shape="circle"
-              ></Button>
-            </Col>
-          </Row>
-          <Row justify={"end"} align={"end"} style={{ marginTop: "20px" }}>
-            <Col style={{ marginRight: "20px" }}>
-              <Button icon={<ShareAltOutlined />} shape="circle"></Button>
-            </Col>
-            <Col>
-              <Button
-                icon={isFullScreen ? <FullscreenExitOutlined /> : <FullscreenOutlined />}
-                shape="circle"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  toggleFullScreen();
-                }}
-              ></Button>
-            </Col>
-          </Row>
-        </div>
-
+        {/* Render flashcard content */}
+        {renderFlashcardContent(isFlippedNormal, setIsFlippedNormal)}
+        {/* Button edit and add to folder */}
         <Row justify={"end"} align={"end"} style={{ marginTop: "20px" }}>
-          <Button icon={<EditOutlined />} shape="circle" style={{ marginRight: "20px" }} onClick={() => navigate(`${CLIENT_URI.EDIT_FLASH_CARD}/${flashcardId}`)}></Button>
+          <Button
+            icon={<EditOutlined />}
+            shape="circle"
+            style={{ marginRight: "20px" }}
+            onClick={() =>
+              navigate(`${CLIENT_URI.EDIT_FLASH_CARD}/${flashcardId}`)
+            }
+          ></Button>
           <Dropdown menu={{ items: folderSelected }} trigger={["click"]}>
-            <Button icon={<FolderOutlined />} shape="circle" style={{ marginRight: "10px" }}></Button>
+            <Button
+              icon={<FolderOutlined />}
+              shape="circle"
+              style={{ marginRight: "10px" }}
+            ></Button>
           </Dropdown>
         </Row>
+        {/* List word and definition */}
         <div style={{ marginTop: "20px", textAlign: "center" }}>
           <TitleCustom level={2}>TỪ ĐIỂN</TitleCustom>
           <List
@@ -256,18 +404,21 @@ export default function FlashCardDetail() {
             renderItem={(item) => (
               <List.Item>
                 <Row style={{ width: "100%" }} align="middle">
-                  <Col style={{ flex: 1, textAlign: "left", paddingLeft: "10px" }}>
+                  <Col
+                    style={{
+                      flex: 1,
+                      textAlign: "left",
+                      paddingLeft: "10px",
+                    }}
+                  >
                     <Button
-                      style={{
-                        marginRight: "15px",
-                      }}
+                      style={{ marginRight: "15px" }}
                       shape="circle"
                       icon={<SoundOutlined />}
                       onClick={() => handleSpeak(item.terms)}
                     />
                     <span style={{ marginRight: "15px" }}>{item.terms}</span>
                   </Col>
-
                   <Col
                     style={{
                       flex: 1,
@@ -277,8 +428,14 @@ export default function FlashCardDetail() {
                       alignItems: "center",
                     }}
                   >
-                    <span style={{ marginRight: "10px" }}>{item.definitions}</span>
-                    <Button shape="circle" icon={<SoundOutlined />} onClick={() => handleSpeak(item.definitions)} />
+                    <span style={{ marginRight: "10px" }}>
+                      {item.definitions}
+                    </span>
+                    <Button
+                      shape="circle"
+                      icon={<SoundOutlined />}
+                      onClick={() => handleSpeak(item.definitions)}
+                    />
                   </Col>
                 </Row>
               </List.Item>
