851d10499a73b9e16798077cd4ba7e2cafb6546b
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-13 11:58:57 +0700

    Add sample data for blog details API

---
+++ frontend/src/pages/GuestsPage/BlogDetails/index.js
@@ -0,0 +1,17 @@
+//sample data from api
+// const dataSample = {
+//   data: {
+//     _id: "6706d47e09b1c0b43cb240fc",
+//     title: "Learning German 123",
+//     description:
+//       "This course is designed for learners aiming to achieve C1 level proficiency in German. It includes detailed lessons on grammar, vocabulary, and advanced communication skills. Additionally, the course offers numerous exercises to reinforce understanding and improve fluency. Each module of the course focuses on real-life scenarios to help learners apply what they’ve learned in practical settings.",
+//     createdBy: "6706bd968b22c99d1f5ccfe5",
+//     status: "Active",
+//     views: 5,
+//     createdAt: "2024-10-09T19:07:42.898Z",
+//     updatedAt: "2024-10-13T04:55:12.850Z",
+//     __v: 0,
+//   },
+//   success: true,
+//   message: "Fetched blog details",
+// };
