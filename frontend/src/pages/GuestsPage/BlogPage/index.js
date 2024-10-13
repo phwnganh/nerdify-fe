f29c10f2670a7678f5d1dcdddfe76739c2ca797e
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-13 10:00:42 +0700

    Create index.js

---
+++ frontend/src/pages/GuestsPage/BlogPage/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const dataSample = {
+  data: [
+    {
+      _id: "6706d47e09b1c0b43cb240fc",
+      title: "Learning German 123",
+      description:
+        "This course is designed for learners aiming to achieve C1 level proficiency in German. It includes detailed lessons on grammar, vocabulary, and advanced communication skills. Additionally, the course offers numerous exercises to reinforce understanding and improve fluency. Each module of the course focuses on real-life scenarios to help learners apply what they’ve learned in practical settings.",
+      createdBy: "6706bd968b22c99d1f5ccfe5",
+      status: "Active",
+      views: 3,
+      createdAt: "2024-10-09T19:07:42.898Z",
+      updatedAt: "2024-10-09T19:09:04.972Z",
+      __v: 0,
+    },
+    {
+      _id: "6706d48209b1c0b43cb240fe",
+      title: "Learning German 456",
+      description:
+        "This course is designed for learners aiming to achieve C1 level proficiency in German. It includes detailed lessons on grammar, vocabulary, and advanced communication skills. Additionally, the course offers numerous exercises to reinforce understanding and improve fluency. Each module of the course focuses on real-life scenarios to help learners apply what they’ve learned in practical settings.",
+      createdBy: "6706bd968b22c99d1f5ccfe5",
+      status: "Active",
+      views: 0,
+      createdAt: "2024-10-09T19:07:46.699Z",
+      updatedAt: "2024-10-09T19:07:46.699Z",
+      __v: 0,
+    },
+  ],
+  success: true,
+  message: "Fetched all blogs",
+};
+
+export const BlogPage = () => {};
+
+export default BlogPage;
