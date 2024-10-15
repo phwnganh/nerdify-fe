1b70fa8bcfebd1ad7b0b4991e9b86a10d9d41a5d
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-15 21:45:28 +0700

    Update AccountantLayout.js

--- frontend/src/layouts/AccountantLayout.js
+++ frontend/src/layouts/AccountantLayout.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Layout } from "antd";
+// test sidebar
+import { AdminContentSidebar } from "../components/SidebarAdmin/SidebarItems";
 
-const AccountantLayout = () => {
-  return <div>AccountantLayout</div>;
+const { Content } = Layout;
+
+export const AccountantLayout = ({ children }) => {
+  const styles = {
+    container: {
+      display: "flex",
+      minHeight: "100vh",
+    },
+    content: {
+      flex: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  };
+
+  return (
+    <div style={styles.container}>
+      <AdminContentSidebar />
+      <Content style={styles.content}>{children}</Content>
+    </div>
+  );
 };
 
 export default AccountantLayout;
