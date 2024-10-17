0f1e07fc364f438622f533e252a9ad027ddfce1a
dnthchung <doanthanhchung03nb@gmail.com>
2024-10-17 08:50:39 +0700

    mdf

--- frontend/src/guards/AccountantGuards.js
+++ frontend/src/guards/AccountantGuards.js
@@ -7,17 +7,13 @@ export const AccountantGuard = ({ children }) => {
   const userInfo = getStorage(STORAGE.USER_INFO);
   console.log("userInfo", userInfo);
 
-  // Check if user information exists in storage
   if (userInfo) {
-    // Verify if the role matches 'accountant'
     if (userInfo?.role === "accountant") {
       return <>{children}</>;
     }
-    // If the role is not 'accountant', redirect to a relevant page
     return <Navigate to={CLIENT_URI.ACCOUNTANT_DASHBOARD} replace />;
   }
 
-  // If user is not authenticated, redirect to the login page
   return <Navigate to={CLIENT_URI.LOGIN} replace />;
 };
 
