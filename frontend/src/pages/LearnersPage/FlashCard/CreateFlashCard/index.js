7aff8e6bc28046324a83757cc7b69f9319629c27
phwnganh <phuonganh25102003@gmail.com>
2024-11-03 20:09:32 +0700

    remove flashcard image

--- frontend/src/pages/LearnersPage/FlashCard/CreateFlashCard/index.js
+++ frontend/src/pages/LearnersPage/FlashCard/CreateFlashCard/index.js
@@ -192,7 +192,7 @@ export default function CreateFlashCard() {
                             />
                           </Form.Item>
                         </Col>
-                        <Col span={4} style={{ margin: "20px 10px 10px 10px" }}>
+                        {/* <Col span={4} style={{ margin: "20px 10px 10px 10px" }}>
                           <Form.Item name={[field.name, "fileList"]} valuePropName="fileList" getValueFromEvent={normFile}>
                             <Upload action="/upload.do" listType="picture-card">
                               <button style={{ border: 0, background: "none" }} type="button">
@@ -201,7 +201,7 @@ export default function CreateFlashCard() {
                               </button>
                             </Upload>
                           </Form.Item>
-                        </Col>
+                        </Col> */}
                       </Row>
                     </div>
                   </Card>
